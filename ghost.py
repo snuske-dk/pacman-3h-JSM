f1e417a099048f61db50c09829824bcd225dc26f
snuske-dk <martinlindhardtm@gmail.com>
2022-11-09 12:55:40 +0100

    not sound

--- ghost.py
+++ ghost.py
@@ -5,13 +5,6 @@ class Ghost:
 
     def __init__(self, row, col):
 
-        # https://sfxr.me/#34T6PktF8TcRjBGBCtaWAp8xrJeEmwSfouC2KVwAWC42iM2UWcDqruxhd8Xq4MFBc7kMaDGuyeyqde9ddiWDHprGh2dvs6Ery9NZQmbQM9gyXmSZzdhxPnMnw
-        # https://sfxr.me/#34T6PkpqAUU8XZ3ze41FCou6ZCuAPdnvQEjkm2P1TPRMxjSRZdiQm9e5DJF1dPTvN8C3gPXJ7DuFniwZVHsmDC5qDkCUYDnkkgQAsqe9MaC2pHxKexVqdd5Jw
-        self.sound_move0 = pg.mixer.Sound("sounds/pacman_move_0.wav")
-        self.sound_move1 = pg.mixer.Sound("sounds/pacman_move_1.wav")
-        self.sound_move0.set_volume(0.5)
-        self.sound_move1.set_volume(0.5)
-
         self.col = col
         self.row = row
 
